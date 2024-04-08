ca9e0d8185150219a77ca32b9c9e0ae7b1c827f0
tanmoyie <tanmoyie@dal.ca>
2024-04-08 01:44:26 -0400

    Streamlit web app UI

--- BIMReTA_app.py
+++ BIMReTA_app.py
@@ -6,38 +6,30 @@ Output: y_test[0]"""
 import joblib
 import pandas as pd
 import numpy as np
-
-# import predict_model
 import streamlit as st
 
-# import model
 model = joblib.load('models/model_BIMReTA.pkl')
-# https://github.com/tanmoyie/Bayesian-Inference-Modeling/blob/clean-code/models/model_BIMReTA.pkl
 
 
 def rank_response_technologies(dispersion, E_ss, E_sl, E_sw, sufficient_mixing_energy,
                                E_ssC, seawater, E_ssI, soot_pollution, displacement):
     """Ranking oil spill response technologies in Arctic
-    ++
-    Motivated from https://github.com/krishnaik06/Dockers/blob/master/app.py
     """
     X1 = pd.DataFrame(np.array([[dispersion, E_ss, E_sl, E_sw, sufficient_mixing_energy,
                                  E_ssC, seawater, E_ssI, soot_pollution, displacement]]))
     # 99, 1, 1, 1, 'no', 0, 'Small', 1, 0, 'yes'
-    X1.columns = ['evaporation_and_natural_disperson', 'E_ss', 'E_sl', 'E_sw','sufficient_mixing_energy',
+    X1.columns = ['evaporation_and_natural_disperson', 'E_ss', 'E_sl', 'E_sw', 'sufficient_mixing_energy',
                   'E_ssC', 'seawater', 'E_ssI', 'soot_pollution', 'displacement']
 
-    prediction = model.predict(X1).reshape(1,-1).flatten().tolist()
+    prediction = model.predict(X1).reshape(1, -1).flatten().tolist()
     return prediction
 
 
 def main():
-    # global result_df
-
     html_temp = """<div style="background-color:tomato;padding:10px;height=20px">
     <h3 style="color:white;text-align:center;">Ranking spill response technology: ML App </h3>
     </div>"""
-    st.markdown(html_temp,  unsafe_allow_html=True)
+    st.markdown(html_temp, unsafe_allow_html=True)
 
     input_title = """<div style="background-color:gray;padding:0px">
     <h4 style="color:white;text-align:center;">Input variables </h4>
@@ -68,9 +60,8 @@ def main():
     if st.button("Rank Technology"):
         result = rank_response_technologies(dispersion, E_ss, E_sl, E_sw, sufficient_mixing_energy,
                                             E_ssC, seawater, E_ssI, soot_pollution, displacement)
-
         result_df1 = pd.DataFrame(columns=["MCR", "CDU", "ISB"])
-        #result_df = pd.concat([result_df1, pd.DataFrame(result)], ignore_index=True, axis=0)
+        # result_df = pd.concat([result_df1, pd.DataFrame(result)], ignore_index=True, axis=0)
         result_df1.loc[len(result_df1.index)] = result
         result_df1.set_index([['Ranking'] * len(result_df1)],
                              inplace=True)  # df.set_index([['A']*len(df)], inplace=True)
@@ -81,6 +72,8 @@ def main():
                 "Marine Pollution Bulletin, 185, 114203 <a href="https://doi.org/10.1016/j.marpolbul.2022.114203"> (link) </a> </p>
     </div>"""
     st.markdown(paper_title, unsafe_allow_html=True)
+    # Motivated from https://github.com/krishnaik06/Dockers/blob/master/app.py
+
 
 if __name__ == '__main__':
     main()
